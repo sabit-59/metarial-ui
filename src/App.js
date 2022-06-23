5904c26ba9ce6cdf4547d705d6f012e82888f27a
jayedbin059@gmail.com <jayedbin059@gmail.com>
2022-06-23 23:07:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { Button } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+
+
 import './App.css';
+import News from './Components/News';
 
 function App() {
+
+  const[articles,setArticles] =useState([]);
+
+  useEffect(()=>{
+    const url ='https://newsapi.org/v2/everything?q=tesla&from=2022-05-22&sortBy=publishedAt&apiKey=de0e722d7b77482bab9a227fad294f05'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setArticles(data.articles)
+      console.log("fetch",data)
+    })
+  }
+  ,[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Headlines:{articles.length}</h2>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
+      <Button> variant="contained" color="success"
+      Success</Button>
+      <News></News>
     </div>
   );
 }
